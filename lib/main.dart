1f38ad05f178093d8ae39216ed74ebedae704818
h4636oh <h4636oh@tuta.io>
2025-02-22 17:32:53 +0530

    disabled ssl certificates

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'screens/chatScreen/chat_screen.dart';
+import 'dart:io';
+
+class HttpOverridesImpl extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
+}
 
 void main() {
+  HttpOverrides.global = HttpOverridesImpl();
   runApp(const MyApp());
 }
 
