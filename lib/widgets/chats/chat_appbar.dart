28851e8c260a7b341e3bff441a2ab7d2493f8948
anomitroid <anomitroid2005@gmail.com>
2025-02-23 12:39:48 +0530

    name chaned to mauli

--- lib/widgets/chats/chat_appbar.dart
+++ lib/widgets/chats/chat_appbar.dart
@@ -34,7 +34,7 @@ class ChatAppBar extends StatelessWidget implements PreferredSizeWidget {
                 borderRadius: BorderRadius.circular(50),
               ),
               child: const Text(
-                "ATHENA",
+                "MAULI",
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                   fontFamily: "nasa",
